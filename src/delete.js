d90cd8fea4ceeb2609fc0d11c2662ac9f9300bde
Mehdibmld <mido.622@outlook.fr>
2025-03-20 10:21:07 +0100

    testfinal

--- src/delete.js
+++ src/delete.js
@@ -3,6 +3,7 @@ import { recipesDB } from "./services.js";
 // Route DELETE /cities/:cityId/recipes/:recipeId
 export const deleteRecipe = (req, res) => {
   const { cityId, recipeId } = req.params;
+  const recipeIdNumber = Number(recipeId); // Convertir en nombre
 
   // Vérifier si la ville existe
   if (!recipesDB[cityId]) {
@@ -10,7 +11,7 @@ export const deleteRecipe = (req, res) => {
   }
 
   // Vérifier si la recette existe
-  const recipeIndex = recipesDB[cityId].findIndex((recipe) => recipe.id == recipeId);
+  const recipeIndex = recipesDB[cityId].findIndex((recipe) => recipe.id === recipeIdNumber);
   if (recipeIndex === -1) {
     return res.status(404).json({ success: false, error: "Recipe not found" });
   }
@@ -18,9 +19,9 @@ export const deleteRecipe = (req, res) => {
   // Supprimer la recette
   recipesDB[cityId].splice(recipeIndex, 1);
 
-  // Vérification après suppression
+  // Supprimer la clé si plus aucune recette n'existe pour cette ville
   if (recipesDB[cityId].length === 0) {
-    delete recipesDB[cityId]; // Supprimer l'entrée si elle est vide
+    delete recipesDB[cityId];
   }
 
   return res.status(204).send(); // No Content
